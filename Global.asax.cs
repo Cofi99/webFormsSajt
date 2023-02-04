fa5796cd1576993cc3fa377a46329e64ab89936d
Cofi99 <123877004+Cofi99@users.noreply.github.com>
2023-02-04 14:07:35 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using IzradaWebFormsSajta.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace IzradaWebFormsSajta
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            RoleAction role = new RoleAction();
+            role.AddUserAndRoles();            
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IzradaWebFormsSajta.Startup))]
+namespace IzradaWebFormsSajta
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
